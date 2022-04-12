2b9ab211c90fd23f11804ce1f2e284818b885d4c
MarlonHDC <marlon.dias@ufpe.br>
2022-04-12 02:46:01 -0300

    Initial commit

---
+++ src/main/java/me/dio/academia/digital/service/impl/AvaliacaoFisicaServiceImpl.java
@@ -0,0 +1,61 @@
+package me.dio.academia.digital.service.impl;
+
+import me.dio.academia.digital.entity.Aluno;
+import me.dio.academia.digital.entity.AvaliacaoFisica;
+import me.dio.academia.digital.entity.form.AvaliacaoFisicaForm;
+import me.dio.academia.digital.entity.form.AvaliacaoFisicaUpdateForm;
+import me.dio.academia.digital.repository.AlunoRepository;
+import me.dio.academia.digital.repository.AvaliacaoFisicaRepository;
+import me.dio.academia.digital.service.IAvaliacaoFisicaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AvaliacaoFisicaServiceImpl implements IAvaliacaoFisicaService {
+
+
+    @Autowired
+    private AvaliacaoFisicaRepository avaliacaoFisicaRepository;
+
+    @Autowired
+    private AlunoRepository alunoRepository;
+
+    @Override
+    public AvaliacaoFisica create(AvaliacaoFisicaForm form) {
+        // Cria uma avaliação física
+        AvaliacaoFisica avaliacaoFisica = new AvaliacaoFisica();
+        Aluno aluno = alunoRepository.findById(form.getAlunoId()).get();
+
+        // Pega os dados do formulário
+        avaliacaoFisica.setAluno((aluno));
+        avaliacaoFisica.setPeso(form.getPeso());
+        avaliacaoFisica.setAltura(form.getAltura());
+
+        // Persiste os dados no bando de dados
+        return avaliacaoFisicaRepository.save(avaliacaoFisica);
+
+
+    }
+
+    @Override
+    public AvaliacaoFisica get(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<AvaliacaoFisica> getAll() {
+        return null;
+    }
+
+    @Override
+    public AvaliacaoFisica update(Long id, AvaliacaoFisicaUpdateForm formUpdate) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+}
