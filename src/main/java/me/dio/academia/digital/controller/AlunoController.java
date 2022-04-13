c8ca811514330142e752c7080e5d99f01cf0e310
MarlonHDC <marlon.dias@ufpe.br>
2022-04-13 08:43:30 -0300

    updated functions

--- src/main/java/me/dio/academia/digital/controller/AlunoController.java
+++ src/main/java/me/dio/academia/digital/controller/AlunoController.java
@@ -8,6 +8,7 @@ import me.dio.academia.digital.service.impl.AlunoServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -17,19 +18,18 @@ public class AlunoController {
     @Autowired
     private AlunoServiceImpl service;
 
-    @GetMapping
-    public List<Aluno> getAll() {
-        return service.getAll();
-    }
-
     @PostMapping
-    public Aluno create(@RequestBody AlunoForm form) {
+    public Aluno create(@Valid @RequestBody AlunoForm form) {
         return service.create(form);
     }
 
     @GetMapping("/avaliacoes/{id}")
     public List<AvaliacaoFisica> getAllAvaliacaoFisicaId(@PathVariable Long id) {
         return service.getAllAvaliacaoFisicaId(id);
+    }
+    @GetMapping
+    public List<Aluno> getAll() {
+        return service.getAll();
     }
 
 
